6100f8923a5b7e310386f6c06f3e235a90e5a0da
artur.sak <asak@apdev01.twopoint.com>
2018-06-17 13:00:34 -0500

    Cleaned up zprezto directory

---
+++ zsh/.zsh/.aliases.zsh
@@ -0,0 +1,22 @@
+###############################################################
+# => Alises
+###############################################################
+alias cp="cp -r"
+alias scp="scp -r"
+alias mkdir="mkdir -p"
+alias tree="tree -C"
+
+alias ..="cd .. && ls"
+alias ...="cd ../.. && ls"
+alias ....="cd ../../.. && ls"
+
+alias reload="exec ${SHELL} -l"
+alias path="echo ${PATH//:/\\n}"
+alias python3="/usr/bin/python36"
+
+alias ls="ls -haF --color=auto"
+alias ll="ls -lahF --color=auto"
+alias lsl="ls -lhF --color=auto"
+
+alias config="vim ~/.zshrc"
+
