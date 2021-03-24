bab0dc5c87d8ee13fa5fae73cd0c9c22164cb5ea
artur-sak13 <artursak1994@gmail.com>
2021-03-23 22:34:31 -0500

    feat: replace zsh shell prompt with spaceship

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -2,14 +2,26 @@
 ###############################################################
 # => Environment
 ###############################################################
+
+export ITERM2_SQUELCH_MARK=1
+# shellcheck source=/dev/null
+[ -s "${HOME}/.iterm2_shell_integration.zsh" ] && \. "${HOME}/.iterm2_shell_integration.zsh"
+
 export ZSH="${HOME}/.oh-my-zsh"
-export ZSH_THEME="lambda-pure"
+export ZSH_THEME="spaceship"
+SPACESHIP_CHAR_PREFIX="%{$(iterm2_prompt_mark)%}"
+export SPACESHIP_CHAR_PREFIX
+export SPACESHIP_CHAR_SYMBOL="Ⓐ"
+export SPACESHIP_CHAR_SUFFIX="  "
+export SPACESHIP_CHAR_COLOR_SUCCESS="cyan"
+# export ZSH_THEME="lambda-pure"
 export ZSH_COMPDUMP="${ZDOTDIR:-$HOME}/.zcompdump"
 export ENABLE_CORRECTION=false
 # Turn off insecure completion dir
 export ZSH_DISABLE_COMPFIX=true
 
-export DOTFILES="${HOME}/.dotfiles"
+export DOTFILES="${HOME}/dotfiles"
+export HOMEBREW_PREFIX="/usr/local"
 
 export PYENV_ROOT="${HOME}/.pyenv"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
@@ -18,25 +30,17 @@ export RBENV_ROOT="${HOME}/.rbenv"
 
 export NVM_DIR="${HOME}/.nvm"
 
-# shellcheck disable=SC1091
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-# shellcheck disable=SC1091
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
-
-export HOMEBREW_PREFIX="/usr/local"
 export EDITOR="${HOMEBREW_PREFIX}/bin/nvim"
-
-export NAME=twopoint.k8s.local
-export ZONES=us-east-2a,us-east-2b,us-east-2c
-export KOPS_STATE_STORE=s3://twopoint-state-store
 
 export AWS_PROFILE=default
 export AWS_REGION=us-east-2
+export SAM_CLI_TELEMETRY=0
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export BAT_PAGER=""
 
 if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
-	GPG_TTY=$(tty)
-	export GPG_TTY
+  GPG_TTY=$(tty)
+  export GPG_TTY
 fi
