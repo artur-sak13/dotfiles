76c0f37cb5c47083e52687530c019e49340f6812
artur-sak13 <artursak1994@gmail.com>
2021-03-23 22:17:04 -0500

    enhancement: add aliases for cat and ls, and remove several deprecated commands

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -2,11 +2,23 @@
 ###############################################################
 # => Alises
 ###############################################################
-alias ls="ls -ahF --color"
+# shellcheck source=/dev/null
+if hash bat 2>/dev/null; then
+  alias cat="bat"
+fi
+
+if hash exa 2>/dev/null; then
+  alias ls="exa -ahF --color always"
+else
+  alias ls="ls -ahF --color"
+fi
+
 alias cp="cp -r"
 alias scp="scp -r"
 alias mkdir="mkdir -p"
 alias tree="tree -C"
+
+alias psusers="ps hax -o user | sort | uniq -c | sort -r"
 
 alias ..="cd .. && ls"
 alias ...="cd ../.. && ls"
@@ -42,8 +54,8 @@ alias credsniff="sudo tcpdump port http or port ftp or port smtp or port imap or
 alias httpdump="sudo tcpdump -i en0 -n -s 0 -w - | grep -a -o -E \"Host\\: .*|GET \\/.*\""
 
 alias vim="nvim"
-alias config="code ~/.dotfiles/zsh/"
-alias dots='code ${DOTFILES}'
+alias code="code-insiders"
+alias dots='code-insiders ${DOTFILES}'
 
 alias untar='tar -xvf'
 alias hieroglyph="echo -e \"\\033(0\""
@@ -57,7 +69,7 @@ if [[ "$OSTYPE" == darwin* ]]; then
   alias cleanupLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
   alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
 
-  alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+  alias flush="sudo killall -HUP mDNSResponder"
   alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
   alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
   alias plistbuddy="/usr/libexec/PlistBuddy"
